32d9ffde11a4a4fe63d12567ed0285b1cd6c8755
SafaeQ <safaedani009@gmail.com>
2022-04-01 11:46:52 +0000

    db

--- api/connection/db.js
+++ api/connection/db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config()
 
 async function db (){
     await mongoose.connect(process.env.DATABASE,{useNewUrlParser:true,useUnifiedTopology:true }, err =>{
-    console.log(' 😊 WooW connected');
+    console.log(" 😊 it's connected");
 })
 }
 
