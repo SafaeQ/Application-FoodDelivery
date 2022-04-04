9df4c5aad2f88a556e6c30720d8a7c92b2c4b9ff
SafaeQ <safaedani009@gmail.com>
2022-04-04 13:39:14 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- api/controllers/leader.controller.js
+++ api/controllers/leader.controller.js
@@ -5,18 +5,9 @@ const bcrypt = require('bcryptjs')
 
 const add_new_chefSecteur = async (req, res) => {
     try {
-        const {name, email, adress, number, password} = req.body
-      console.log(req.body);
-        const emailExist = await Leader.findOne({ email });
 
-        if(emailExist) return res.status(400).send("Email Already Exist!");
-      console.log('----------');
-        const hashedPassword = await bcrypt.hash(password);
-  
-        const leader = { name, password: hashedPassword, email, adress, number }
-// console.log('leader',leader);
-        const newLeader = Leader.create(leader)
-        
+        const newLeader = await Leader.create(req.body)
+        // console.log(newLeader);
         const result = await newLeader.save()
 
         res.status(200).send(result)
