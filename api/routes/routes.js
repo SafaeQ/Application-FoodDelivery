2f5bb693c2683978c4549bda93d71d3029b2b083
SafaeQ <safaedani009@gmail.com>
2022-04-05 15:15:03 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,6 +1,4 @@
-const express = require('express')
-
-const router =express.Router()
+const router =require('express').Router()
 
 const {signUp, login} = require('../controllers/user.controller')
 
