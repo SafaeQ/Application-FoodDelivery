b639d18b85c26a78ce2eb9e19037ec3211ebb2c8
SafaeQ <safaedani009@gmail.com>
2022-04-05 14:36:29 +0000

    ✔🐱‍👤 Application-FoodDelivery

---
+++ api/controllers/restaurant.controller.js
@@ -0,0 +1,17 @@
+const Restaurant = require('../models/restaurant.model')
+
+
+const add_restaurant = async (req, res, next) => {
+    
+    try{
+        const restaurant = await Restaurant.create(req.body)
+
+        const new_restaurant = await restaurant.save() 
+
+        res.status(200).send(new_restaurant)
+
+    }catch (error) {
+
+        res.status(404).send(error)
+    }    
+}
