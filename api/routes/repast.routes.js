96db75a670aa9c6854ae2499447d8ef726ecd0bb
SafaeQ <safaedani009@gmail.com>
2022-04-05 15:44:50 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- api/routes/repast.routes.js
+++ api/routes/repast.routes.js
@@ -2,12 +2,16 @@ const repastRouter = require('express').Router()
 
 const {add_Repast, getAll_Repast, delete_repast } = require('../controllers/repast.controller')
 
+const isAuthorized = require('../middlewares/permission')
 
-    repastRouter.post('/add-repast', add_Repast)
+const checkAuthentication = require('../middlewares/auth')
+
+
+    repastRouter.post('/add-repast', checkAuthentication, isAuthorized('leader'), add_Repast)
 
     repastRouter.get('/all-repast', getAll_Repast)
 
-    repastRouter.delete('/delete-repast/:id', delete_repast)
+    repastRouter.delete('/delete-repast/:id',checkAuthentication, isAuthorized('leader'), delete_repast)
 
 
 module.exports = repastRouter
