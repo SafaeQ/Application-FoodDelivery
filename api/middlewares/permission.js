ffd241ab0ecf5049746ac56aa3b4923804a1de75
SafaeQ <safaedani009@gmail.com>
2022-04-05 10:28:16 +0000

    ✔🐱‍👤 Application-FoodDelivery

---
+++ api/middlewares/permission.js
@@ -0,0 +1,22 @@
+const isAuthorized = (...role) => {
+
+    return (req, res, next) => {
+
+        console.log("role ", req.user.role.name)
+
+        if (!role.includes(req.user.role.name)) {
+
+            res.status(401).json({
+
+                error: "you are not authorized"
+
+            });
+        } else {
+
+            next()
+        }
+    }
+
+}
+
+module.exports = isAuthorized
