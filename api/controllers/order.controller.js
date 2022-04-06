03249614aca51c15f48a3db4cfd3f59061c422bd
SafaeQ <safaedani009@gmail.com>
2022-04-06 14:09:25 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -1,2 +1,24 @@
-const Order = require('../models/order.model')
+const { Order } = require('../models/order.model')
 
+
+const getUserOrders = async (req, res) => {
+
+    if (req.user) {
+
+        const orders = await Order.find({ user: req.user._id }).sort('-createdAt')
+
+        orders ? res.json(orders) : res.status(404).send('Order Not Found')
+
+    }else {
+
+        res.status(404)
+
+        throw new Error('No User Found')
+    }
+}
+
+const createOrder = async (req, res) => {
+
+}
+
+module.exports = { getUserOrders }
