2482dc5b1bda61d7f9800761605097810411602d
SafaeQ <safaedani009@gmail.com>
2022-04-06 12:55:37 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- api/models/order.model.js
+++ api/models/order.model.js
@@ -1,83 +1,60 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-// const STATES = ["enviado", "aceptado", "despachado", "entregado", "liquidado"];
-
-// const orderSchema = new Schema(
-//   {
-//     orderID: { 
-//         type: Number,
-//         default: Date.now
-//     },
-
-//     client: [
-//       {
-//         type: Schema.Types.ObjectId,
-//         ref: "User",
-//       },
-//     ],
-
-//     description: [
-//       {
-//         product: {
-//           name: {
-//               type: String,
-//               require: true,
-//               lowercase: true
-//             },
-//           price: {
-//             type: Number,
-//             default: 0
-//         },
-//         },
-//         quantity: { 
-//             type: Number,
-//             default: 1
-//         },
-//         total: { 
-//             type: Number,
-//             default: 0
-//         },
-//       },
-//     ],
-
-//     total: { 
-//         type: Number,
-//         default: 0 
-//     },
-
-//     states: [
-//       {
-//         name: { 
-//             type: String,
-//             default: "" 
-//         },
-//         confirmed: { 
-//             type: Boolean,
-//             default: false 
-//         },
-//         date: { 
-//             type: Date,
-//             default: Date.now()
-//         },
-//       },
-//     ],
-//     finished: { 
-//         type: Boolean,
-//         default: false
-//     },
-//   },
-
-//   {
-//     timestamps: true,
-//     versionKey: false,
-//   }
-// );
+const orderSchema = new Schema(
+    {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "User",
+        },
+        orderItems: [
+          {
+            name: { type: String, required: true },
+            qty: { type: Number, required: true },
+            image: { type: String, required: true },
+            price: { type: Number, required: true },
+            food: {
+              type: mongoose.Schema.Types.ObjectId,
+              required: true,
+              ref: "Repast",
+            },
+          },
+        ],
+        shippingAddress: {
+          address: { type: String, required: true },
+          city: { type: String, required: true },
+        },
+        shippingPrice: {
+          type: Number,
+          default: 0.0,
+        },
+        phoneNumber: {
+          type: String,
+          required: true,
+        },
+        totalPrice: {
+          type: Number,
+          required: true,
+          default: 0.0,
+        },
+        isDelivered: {
+          type: Boolean,
+          default: false,
+        },
+        deliveredAt: {
+          type: Date,
+        },
+      },
+      {
+        timestamps: true,
+      }
+);
 
 
-// const Order = mongoose.model("Order", orderSchema);
+const Order = mongoose.model("Order", orderSchema);
 
-// module.exports = { 
-//     Order, STATES 
-// };
+module.exports = { 
+    Order, STATES 
+};
