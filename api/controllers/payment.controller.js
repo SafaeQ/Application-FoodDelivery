49c5f5e67e707d6ac640b9c6d22585fd2fa40359
SafaeQ <safaedani009@gmail.com>
2022-04-08 11:56:27 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- api/controllers/payment.controller.js
+++ api/controllers/payment.controller.js
@@ -1,1 +1,27 @@
+require('dotenv').config()
+
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+
+const {User} = require('../models/user.model')
+
+const create_payment = async (req, res) => {
+    try {
+        
+        const client = await stripe.paymentMethods.create({
+            payment_methods_types : 'card',
+            mode: 'payment',
+            card: {
+                number: '4242424242424242',
+                exp_month: 4,
+                exp_year: 2023,
+                cvc: '314',
+            
+
+        })
+
+
+    } catch (error) {
+
+        res.status(500).json({ error: error.message })
+    }
+}
