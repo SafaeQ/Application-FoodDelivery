8ee9381dfbce24c11950dda4ebf07387665ee3be
SafaeQ <safaedani009@gmail.com>
2022-04-11 09:48:10 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -1,65 +1,12 @@
 import React from 'react'
 
-import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
   
     return (
       <>
-       <nav>
-        <ul>
-          <li>
-            <NavLink
-              className
-              to="/welcome"
-            >
-              Welcome
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className
-              to="/about"
-            >
-              About
-            </NavLink>
-          </li>
-            <li>
-              <NavLink
-                className
-                to="/sign-up"
-              >
-                SignUp
-              </NavLink>
-            </li>
-        
-            <li>
-              <NavLink
-                className
-                to="/login"
-              >
-                Login
-              </NavLink>
-            </li>
-    
-            <li>
-              <a>
-                Logout
-              </a>
-            </li>
-         
-            <li>
-              <NavLink
-                className
-                to="/profile"
-              >
-                profile
-              </NavLink>
-            </li>
-     
-        </ul>
-      </nav>
+       
       </>
     )
 }
