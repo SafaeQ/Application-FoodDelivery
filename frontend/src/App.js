4a2a85f53f6f8dbc94341540004c828127f83d4a
SafaeQ <safaedani009@gmail.com>
2022-04-11 14:30:05 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
 import Navbar from './components/NavBar/NavBar';
 import Dashboard from './pages/DashboardDelevery';
-import { Router, Route, Switch } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
   return (
     <>
-    <Router>
-      <Switch>
-        <Route path='/' render={<Home />} />
-      </Switch>
+    <Router>      
+        <Route path='/' render={<Home />} />      
     </Router>
     <Navbar/>
     <Dashboard />
