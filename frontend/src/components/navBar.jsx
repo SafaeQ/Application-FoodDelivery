32a3d6337c456225117d01c70e4a16d79072f578
SafaeQ <safaedani009@gmail.com>
2022-04-11 10:44:05 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -1,24 +1,11 @@
-import React from 'react'
-import {Nav,Container } from 'react-bootstrap'
+import React from 'react';
 
-
-const Navbar = () => {
-  
-    return (
-       <>
-          <Navbar bg="dark" variant="dark">
-            <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-            </Container>
-          </Navbar>
-        </>
-
-    )
+const navBar = () => {
+  return (
+    <div>
+      
+    </div>
+  );
 }
 
-export default Navbar
+export default navBar;
