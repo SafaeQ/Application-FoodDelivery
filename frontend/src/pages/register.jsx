a8345cd91ec5b4acbfe9e467d1d0068bc2171f9c
SafaeQ <safaedani009@gmail.com>
2022-04-12 11:35:31 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -6,7 +6,8 @@ import axios from 'axios'
 const Register = () => {
   const [user, setUser] = useState([]);
   const [error, setError] = useState(false);
-  
+  // const [submitted, setSubmitted] = useState(false);
+
   const handleChange = (event) => {
     const {name, value} = event.target
     setUser({...user, [name]: value})
@@ -14,20 +15,20 @@ const Register = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-
-    console.log('clicked');
-
     const {name, email, password, adress, number, role} = user
 
+    console.log('clicked');
     if (name=== '' || email === '' || password === '' || adress === '' || number === '' || role) {
 
       setError(true)
 
     }else {
-      axios.post('http://localhost:8000/auth/signup',user)
-            .then(res => console.log( 'dddd',res))
+      const response = axios.post('http://localhost:9988/auth/signup',user)
+      console.log(user);
+      // window.location.href = ('/login')
     }
   }
+
 
   return (
     <>
@@ -36,13 +37,24 @@ const Register = () => {
             <div className="d-flex flex-column">
             <div className="form-group">
             <div className="form-group">
+            <label htmlFor="name" className="form-label m-2 h5">Name</label>
+            <input type="text" name="name" placeholder="Name" className="form-control" onChange={handleChange} required/></div>
+            <div className="form-group">
             <label htmlFor="email" className="form-label m-2 h5">Email</label>
-            <input type="email" name="mail" placeholder="Email" className="form-control"  required/></div>
+            <input type="email" name="email" placeholder="Email" className="form-control" onChange={handleChange} required/></div>
             <div className="form-group">
             <label htmlFor="password" className="form-label m-2 h5">Password</label>
-            <input type="password" name="psd" placeholder="Password" className="form-control"  required/></div>
+            <input type="password" name="password" placeholder="password" className="form-control" onChange={handleChange} required/></div>
+            <div className="form-group">
+            <label htmlFor="number" className="form-label m-2 h5">Number</label>
+            <input type="number" name="number" placeholder="number" className="form-control" onChange={handleChange} required/></div>
+            <div className="form-group">
+            <label htmlFor="role" className="form-label m-2 h5">Role</label>
+            <input type="role" name="role" placeholder="role" className="form-control" onChange={handleChange} required/></div>
+
             <label htmlFor="address" className="form-label m-2 h5">Address</label>
-            <textarea type="text" name="addr" placeholder="Address" className="form-control"  required/></div>
+            <input type="text" name="adress" placeholder="Address" className="form-control" onChange={handleChange} required/></div>
+
             <button className="btn btn-primary my-3 h4">Sign Up</button>
             <div className="h5 text-center" ></div>
             <hr/>
