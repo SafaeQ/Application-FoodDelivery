2dcf9900e2f8a0787dddc14f86309354c6fc6514
SafaeQ <safaedani009@gmail.com>
2022-04-12 13:55:46 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,14 +5,13 @@ const Login = () => {
   return (
     <>
       <form className="form1" >
-            <h2>Login</h2><br/>
-            <div className="form-group d-flex flex-column g-2">
-                
+        <h2>Login</h2><br/>
+        <div className="form-group d-flex flex-column g-2">
         <label htmlFor="email" className="form-label m-2 h5">Email</label>
-        <input type="email" name="mail" className="form-control-lg"   placeholder="Email" required/></div>
+        <input type="email" name="email" className="form-control-lg"  placeholder="Email" required/></div>
         <div className="form-group d-flex flex-column g-2">
         <label htmlFor="password" className="form-label m-2 h5">Password</label>
-        <input type="password" name="psd" className="form-control-lg"  placeholder="Password" required/>
+        <input type="password" name="password" className="form-control-lg"  placeholder="Password" required/>
         </div>
         <button className="btn btn-primary btn-lg m-3 ms-0">Sign in</button>
         <hr/>
