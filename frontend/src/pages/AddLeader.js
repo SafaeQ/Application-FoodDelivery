52b7f912d021d21c7c654c9913c82376b45447ec
SafaeQ <safaedani009@gmail.com>
2022-04-13 10:15:40 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- frontend/src/pages/AddLeader.js
+++ frontend/src/pages/AddLeader.js
@@ -1,58 +1,86 @@
-import React from 'react';
+import React, {Component} from 'react';
 import api from '../components/api';
-import { useState } from 'react';
 
-const AddLeader = (props) => {
 
-  const [leader, setLeader] = useState([])
-
-    const handleInputChange = (event)=> {
-        console.log(event)
-        const {name, value} = event.target
-        setLeader(values => ({...values, [name]: value}))
-    }
-
-    const handleSubmit = async (e)=> {
-        e.preventDefault();
-        console.log('its work')
-      const response = await api.post('/api/add-leader')
-      console.log(response);
-      setLeader([...leader, response.data])
-      if (response.ok) {
-        window.location.href = 'dashboard-leaders'
-      }else {
-        window.location.href = 'add-leader'
+export default class CreateUser extends Component {
+      constructor(props) {
+          super(props)
+          this.onChangeUserName = this.onChangeUserName.bind(this);
+          this.onChangeUserEmail = this.onChangeUserEmail.bind(this);
+          this.onChangeUserPassword = this.onChangeUserPassword.bind(this);
+          this.onChangeUserNumber = this.onChangeUserNumber.bind(this);
+          this.onChangeUserAdress = this.onChangeUserAdress.bind(this);
+          this.onSubmit = this.onSubmit.bind(this);
+          this.state = {
+              name: '',
+              email: '',
+              password: '', 
+              number: '',
+              adress: ''
+          }
       }
-        
-    }
+      onChangeUserName(e) {
+          this.setState({ name: e.target.value })
+      }
+      onChangeUserEmail(e) {
+          this.setState({ email: e.target.value })
+      }
+      onChangeUserPassword(e) {
+          this.setState({ password: e.target.value })
+      }
+      onChangeUserNumber(e) {
+          this.setState({ number: e.target.value })
+      }
+      onChangeUserAdress(e) {
+          this.setState({ adress: e.target.value })
+      }
+      onSubmit(e) {
+          e.preventDefault()
+          const userObject = {
+              name: this.state.name,
+              email: this.state.email,
+              password: this.state.password,
+              number: this.state.number,
+              adress: this.state.adress
+          };
+        api.post('api/add-leader', userObject)
+              .then((res) => {
+                  console.log(res.data)
+              }).catch((error) => {
+                  console.log(error)
+              });
+          this.setState({ name: '', email: '', password:'', number:'', adress:'' })
+      }
+  render(h) {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={this.onSubmit}>
         <label>
           Name:
-          <input type="text" name='name' value={leader.name} onChange = {handleInputChange}/>
+          <input type="text" name='name' value={this.state.name} onChange={this.onChangeUserName}/>
         </label>
         <label>
           Email:
-          <input type="email" name='email' value={leader.email} onChange = {handleInputChange}/>
+          <input type="email" name='email' value={this.state.email} onChange={this.onChangeUserEmail}/>
         </label>
         <label>
         Adress:
-          <input type="text" name='adress' value={leader.adress} onChange = {handleInputChange}/>
+          <input type="text" name='adress' value={this.state.adress} onChange={this.onChangeUserAdress}/>
         </label>
         <label>
         Number:
-          <input type="number" name='number' value={leader.number} onChange = {handleInputChange}/>
+          <input type="number" name='number' value={this.state.number} onChange={this.onChangeUserNumber}/>
         </label>
         <label>
         Password:
-        <input type="password" name='password' value={leader.password} onChange = {handleInputChange}/>
+        <input type="password" name='password' value={this.state.password} onChange={this.onChangeUserPassword}/>
         </label>
         <input type="submit" value="Submit" />
       </form>
     </>
-  );
+  )
+}
 }
 
-export default AddLeader;
+// export default AddLeader;
