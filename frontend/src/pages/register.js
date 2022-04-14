07c020d9685f79b55b2aa0de5e06ba8f846d7d1c
SafaeQ <safaedani009@gmail.com>
2022-04-14 15:06:55 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -61,7 +61,7 @@ const Register = ()=> {
   return (
     <>
     <div className="container">
-      <form className="form1" >
+      <form className="form1"  onSubmit = {handleSubmit}>
             <h2 className="text-center">Register Form</h2>
             <br/>
             <div className="container h-100">
@@ -70,24 +70,24 @@ const Register = ()=> {
             
             <div className="form-group">
             <label htmlFor="name" className="form-label m-2 h5">Name</label>
-            <input type="text" name="name" placeholder="Name" className="form-control" onChange={(e) => handleInputChange(e)} /></div>
+            <input type="text" name="name" placeholder="Name" className="form-control" onChange={ handleInputChange } /></div>
             <div className="form-group">
             <label htmlFor="email" className="form-label m-2 h5">Email</label>
-            <input type="email" name="email" placeholder="Email" className="form-control" onChange={(e) => handleInputChange(e)} /></div>
+            <input type="email" name="email" placeholder="Email" className="form-control" onChange={ handleInputChange } /></div>
             <div className="form-group">
             <label htmlFor="password" className="form-label m-2 h5">Password</label>
-            <input type="password" name="password" placeholder="password" className="form-control" onChange={(e) => handleInputChange(e)} /></div>
+            <input type="password" name="password" placeholder="password" className="form-control" onChange={ handleInputChange } /></div>
             <div className="form-group">
             <label htmlFor="number" className="form-label m-2 h5">Number</label>
-            <input type="number" name="number" placeholder="number" className="form-control" onChange={(e) => handleInputChange(e)} /></div>
+            <input type="number" name="number" placeholder="number" className="form-control" onChange={ handleInputChange } /></div>
             <div className="form-group">
             <label htmlFor="role" className="form-label m-2 h5">Role</label>
-            <input type="role" name="role" placeholder="role" className="form-control" onChange={(e) => handleInputChange(e)} /></div>
+            <input type="role" name="role" placeholder="role" className="form-control" onChange={ handleInputChange } /></div>
 
             <label htmlFor="address" className="form-label m-2 h5">Address</label>
-            <input type="text" name="adress" placeholder="Address" className="form-control" onChange={(e) => handleInputChange(e)} /></div>
+            <input type="text" name="adress" placeholder="Address" className="form-control" onChange={ handleInputChange } /></div>
 
-            <button className="btn btn-primary my-3 h4" onSubmit = {()=>handleSubmit()}>Sign Up</button>
+            <button className="btn btn-primary my-3 h4" >Sign Up</button>
             <div className="h5 text-center" ></div>
             <hr/>
             <span>Already have an account?  |  <Link to="/login">Sign in</Link></span>              
