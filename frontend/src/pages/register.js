0941f2a24e058009bb2a457dd732eaba01311328
SafaeQ <safaedani009@gmail.com>
2022-04-14 14:58:19 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -1,52 +1,67 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios'
+import React, {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom'
 import api from '../components/api';
 
-class Register extends React.Component {
+const Register = ()=> {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [number, setNumber] = useState('');
+    const [adress,setAdress] = useState('');
+    const [role,setRole] = useState('');
 
-  state = {
-    name: '',
-    email:'',
-    password:'',
-    number: '',
-    role: '',
-    adress: ''
-}
+    const navigate = useNavigate();
 
-  handleChange = (event) => {
-    const {name, value} = event.target
-    this.setState({ [name]: value})
-  }
+    const handleInputChange = (e) => {
+        const {name , value} = e.target;
 
-  handleSubmit = (e) => {
+        if(name === "name"){
+            setName(value);
+        }
+        if(name === "number"){
+            setNumber(value);
+        }
+        if(name === "email"){
+            setEmail(value);
+        }
+        if(name === "password"){
+            setPassword(value);
+        }
+        if(name === "adress"){
+          setAdress(value);
+        }
+        if(name === "role"){
+          setRole(value);
+        }
+
+      }
+    const handleSubmit = (e) => {
       e.preventDefault()
       console.log('clicked');
-      const user = {
-        name: this.state.name,
-        email: this.state.email,
-        password: this.state.password,
-        number: this.state.number,
-        adress: this.state.adress,
-        role: this.state.role
-    };
+        let obj = {
+          name : name,
+          email:email,
+          password:password,
+          adress:adress,
+          number:number,
+          role:role,
+        }
+      console.log(obj);
 
-    console.log(user);
-    api.post('/auth/signup', user)
-        .then((res) => {
-          console.log(res.data)
-          
-      }).catch((error) => {
-          console.log(error)
-      });
-    this.setState({ name: '', email: '', password:'', number:'', adress:'', role:'leader' })
+      api.post('/auth/signup', obj)
+          .then((res) => {
+            console.log(res.data)
+            navigate('/login')
+        }).catch((error) => {
+            console.log(error)
+            navigate('/')
+        });
+
   }
-
-  render() {
   return (
     <>
     <div className="container">
-      <form className="form1" onSubmit={this.handleSubmit}>
+      <form className="form1" >
             <h2 className="text-center">Register Form</h2>
             <br/>
             <div className="container h-100">
@@ -55,24 +70,24 @@ class Register extends React.Component {
             
             <div className="form-group">
             <label htmlFor="name" className="form-label m-2 h5">Name</label>
-            <input type="text" name="name" placeholder="Name" className="form-control" onChange={this.handleChange} required/></div>
+            <input type="text" name="name" placeholder="Name" className="form-control" onChange={(e) => handleInputChange(e)} /></div>
             <div className="form-group">
             <label htmlFor="email" className="form-label m-2 h5">Email</label>
-            <input type="email" name="email" placeholder="Email" className="form-control" onChange={this.handleChange} required/></div>
+            <input type="email" name="email" placeholder="Email" className="form-control" onChange={(e) => handleInputChange(e)} /></div>
             <div className="form-group">
             <label htmlFor="password" className="form-label m-2 h5">Password</label>
-            <input type="password" name="password" placeholder="password" className="form-control" onChange={this.handleChange} required/></div>
+            <input type="password" name="password" placeholder="password" className="form-control" onChange={(e) => handleInputChange(e)} /></div>
             <div className="form-group">
             <label htmlFor="number" className="form-label m-2 h5">Number</label>
-            <input type="number" name="number" placeholder="number" className="form-control" onChange={this.handleChange} required/></div>
+            <input type="number" name="number" placeholder="number" className="form-control" onChange={(e) => handleInputChange(e)} /></div>
             <div className="form-group">
             <label htmlFor="role" className="form-label m-2 h5">Role</label>
-            <input type="role" name="role" placeholder="role" className="form-control" onChange={this.handleChange} required/></div>
+            <input type="role" name="role" placeholder="role" className="form-control" onChange={(e) => handleInputChange(e)} /></div>
 
             <label htmlFor="address" className="form-label m-2 h5">Address</label>
-            <input type="text" name="adress" placeholder="Address" className="form-control" onChange={this.handleChange} required/></div>
+            <input type="text" name="adress" placeholder="Address" className="form-control" onChange={(e) => handleInputChange(e)} /></div>
 
-            <button className="btn btn-primary my-3 h4">Sign Up</button>
+            <button className="btn btn-primary my-3 h4" onSubmit = {()=>handleSubmit()}>Sign Up</button>
             <div className="h5 text-center" ></div>
             <hr/>
             <span>Already have an account?  |  <Link to="/login">Sign in</Link></span>              
@@ -82,8 +97,6 @@ class Register extends React.Component {
         </form>
         </div>
     </>
-  );
+  )
 }
-}
-
-export default Register;
+export default Register
