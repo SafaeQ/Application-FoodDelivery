9235ec29fc183ed2afc4f2682fedd7bcb9f609c0
SafaeQ <safaedani009@gmail.com>
2022-04-14 15:49:10 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -2,4 +2,8 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: 'http://localhost:9988',
+    headers: {
+        'Authorization': "JWT_TOKEN",
+        'Content-Type': 'application/json'
+      }
 });
