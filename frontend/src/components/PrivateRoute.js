c3aa8b846ff3021457ca84f645c958863d1c514e
SafaeQ <safaedani009@gmail.com>
2022-04-18 12:58:57 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({ user, children }) => {
+const PrivateRoute = ({ user, redirectPath = '/login', children }) => {
     if (!user) {
-      return <Navigate to="/login" replace />;
+      return <Navigate to={redirectPath} replace />;
     }
   
     return children;
