924b1feb324047725e60135c7f8d65f97ec28cea
SafaeQ <safaedani009@gmail.com>
2022-04-18 12:59:20 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,11 +14,11 @@ function App() {
     <>
     <Navbar />
     <Routes>      
-        <Route path='/' element={ 
+        <Route path='/' element={
             <PrivateRoute user={'leader'}>
               <Home />
             </PrivateRoute> 
-          } />      
+          } /> 
         <Route path='/login' element={<Login />} />      
         <Route path='/register' element={<Register />} />      
         <Route path='/dashboard-livreur' element={<DashboardDelevery />} />      
