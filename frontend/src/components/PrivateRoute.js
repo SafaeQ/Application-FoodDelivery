30da6ea992308d66db8642b754bb91bdd5e00e29
SafaeQ <safaedani009@gmail.com>
2022-04-18 14:05:38 +0000

    ✔🐱‍👤 Application-FoodDelivery

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+// import React from 'react';
+// import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({ user, redirectPath = '/login', children }) => {
-    if (!user) {
-      return <Navigate to={redirectPath} replace />;
-    }
+// const PrivateRoute = ({ user, children }) => {
+//     if (!user) {
+//       return <Navigate to="/login" replace />;
+//     }
   
-    return children;
-  };
- export default PrivateRoute;
+//     return children;
+//   };
+//  export default PrivateRoute;
+
+import React, { Fragment } from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+const { loading, isAuthenticated } = useSelector((state) => state.user);
+
+if (loading) return null;
+
+return isAuthenticated ? children : <Navigate to="/login" replace />;
+};
+
+export default ProtectedRoute;
+
