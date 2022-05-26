9312e1a3e337c06be09700bf86f1b126ccb089a7
SafaeQ <safaedani009@gmail.com>
2022-05-26 14:08:02 +0100

    ✔🐱‍👤 Application-FoodDelivery

--- api/routes/repast.routes.js
+++ api/routes/repast.routes.js
@@ -8,19 +8,7 @@ const isAuthorized = require('../middlewares/permission')
 
 const checkAuthentication = require('../middlewares/auth')
 
-// const multer  = require('multer')
-
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, './image')
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
-//     }
-//   })
-  
-//   const upload = multer({ storage: storage })
-                 
+                
 
 const storage=multer.diskStorage({
     destination: (req, file, cb) =>{
